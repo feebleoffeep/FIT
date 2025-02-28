8c1b30a4c0a7f982cbfc484c1c6c1bcdfb760f16
koliakrutoy <koliakrutoy78@gmail.com>
2025-02-28 13:43:28 +0200

    Initial Commit

---
+++ FIT/FITApp.swift
@@ -0,0 +1,17 @@
+//
+//  FITApp.swift
+//  FIT
+//
+//  Created by Коля Крутой on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FITTests/FITTests.swift
@@ -0,0 +1,17 @@
+//
+//  FITTests.swift
+//  FITTests
+//
+//  Created by Коля Крутой on 28.02.2025.
+//
+
+import Testing
+@testable import FIT
+
+struct FITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
